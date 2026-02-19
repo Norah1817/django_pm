dc04c244efeae45c5495023f3a37caf7726a197b
norah <gr2040@hotmail.com>
2026-02-19 03:51:43 +0300

    Admin Panel

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -134,3 +139,4 @@ INTERNAL_IPS = [
     '127.0.0.1',
     #...
 ]
+
